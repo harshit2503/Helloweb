0cfd0ecd2a0be3e17bdad1cd626dff3277b8eafc
harshit2503 <113522267+harshit2503@users.noreply.github.com>
2025-04-07 10:16:09 +0530

    Update main.tf

--- terrraform-dotnet/main.tf
+++ terrraform-dotnet/main.tf
@@ -3,43 +3,6 @@ provider "azurerm" {
   subscription_id = "fa5da814-c6c2-422a-be88-0fc464adece6"
 }
 
-# Create Resource Group
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-dot-resource"
-  location = "East US 2"
-}
-
-# Create App Service Plan
-resource "azurerm_service_plan" "asp" {
-  name                = "jenkins-app-service-plan"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  os_type             = "Linux"
-  sku_name            = "B1"  # Fixed the incorrect SKU block
-}
-
-# Create App Service for Jenkins
-resource "azurerm_linux_web_app" "jenkins" {
-  name                = "dot-app-service"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  service_plan_id     = azurerm_service_plan.asp.id
-
-  site_config {
-    always_on = true
-
-    application_stack {
-      dotnet_version = "8.0"  # Fixed the incorrect field name
-    }
-  }
-}
---------------------------------------------------------------
-
-provider "azurerm" {
-  features {}
-  subscription_id = "fa5da814-c6c2-422a-be88-0fc464adece6"
-}
-
 resource "azurerm_resource_group" "rg" {
   name     = "rg-dot-resource"
   location = "East US 2"
@@ -57,9 +20,9 @@ resource "azurerm_app_service_plan" "app_service_plan" {
 }
 
 resource "azurerm_app_service" "my-app_service" {
-  name                = "integrated-dot-net"
-  location            = azurerm_resource_group.rgas.location
-  resource_group_name = azurerm_resource_group.rgas.name
+  name                = "dot-app-service"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
 
   site_config {
