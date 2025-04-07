39cb46b9fbeb958de24d458d5e2fb96b619ed692
harshit2503 <113522267+harshit2503@users.noreply.github.com>
2025-04-07 10:14:27 +0530

    Update main.tf

--- terrraform-dotnet/main.tf
+++ terrraform-dotnet/main.tf
@@ -33,3 +33,47 @@ resource "azurerm_linux_web_app" "jenkins" {
     }
   }
 }
+--------------------------------------------------------------
+
+provider "azurerm" {
+  features {}
+  subscription_id = "fa5da814-c6c2-422a-be88-0fc464adece6"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-dot-resource"
+  location = "East US 2"
+}
+
+resource "azurerm_app_service_plan" "app_service_plan" {
+  name                = "harsh_app_service_plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+resource "azurerm_app_service" "my-app_service" {
+  name                = "integrated-dot-net"
+  location            = azurerm_resource_group.rgas.location
+  resource_group_name = azurerm_resource_group.rgas.name
+  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
+  }
+}
